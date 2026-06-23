af16ed3c86b126a45688f8cbcb57005b2510a698
macielmax <maciel.santos@fatec.sp.gov.br>
2026-06-22 22:53:14 -0300

    barra de progresso

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const currentProgress = document.getElementById ('current-progress');
+const progressContainer = document.getElementById ('progress-container');
 
 const possoclamar = {
     songName : 'Posso Clamar',
@@ -80,10 +82,23 @@ function nextSong(){
     
 }
 
+function updateProgressBar(){
+    const barWidth = (song.currentTime/song.duration)*100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+}
+
+function jumpTo(event){
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition/width)* song.duration;
+    song.currentTime = jumpToTime;
+}
+
 initializeSong();
 
 play.addEventListener('click', playPauseDecide);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
-
+song.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', jumpTo);
 
